d6a35dbdb9ab63d7772a4f7ac38d14c5ca41ff67
Duko Wolf <dukowolf@gmail.com>
2025-02-10 15:49:42 +0100

    Betere index.php

--- Index.php
+++ Index.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel</title>
-    <h1> hotel <h1> 
-        <h2> welkom in het hotel <h2>
+
 
 </head>
+
 <body>
-    
+    <!--  change to @include -->
+    <header>
+        <h1>Bonjour</h1>
+        <h2>Welkom in het hotel</h2>
+    </header>
 </body>
+
 </html>
