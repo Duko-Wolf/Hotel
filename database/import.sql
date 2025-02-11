12c55c2c33ff03911e2bf092b6e5eb7d79912cf3
Duko Wolf <dukowolf@gmail.com>
2025-02-11 16:13:14 +0100

    update import

--- database/import.sql
+++ database/import.sql
@@ -5,8 +5,14 @@ CREATE DATABASE hotel;
 USE hotel;
 
 CREATE TABLE kamers (
-    kamersID int NOT NULL PRIMARY KEY,
+    kamersID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     kamerNaam VARCHAR(255) NOT NULL,
     kamerBeschrijving VARCHAR(255),
     beschikbaarheid BOOLEAN NOT NULL
+);
+
+CREATE TABLE admin (
+    adminID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    adminEmail VARCHAR(255) NOT NULL,
+    
 );
