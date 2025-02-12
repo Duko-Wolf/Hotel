9f7ee12f9a08e4ebc4fc67b0430bc3e04c2b7e81
Duko Wolf <dukowolf@gmail.com>
2025-02-12 09:16:07 +0100

    Vul met info

--- Index.php
+++ Index.php
@@ -15,6 +15,41 @@
         <h1>Bonjour</h1>
         <h2>Welkom in het hotel</h2>
     </header>
+
+    <!-- change to @include -->
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Kamers</a>
+        <a href="#">Restaurant</a>
+        <a href="#">Over ons</a>
+        <a href="#">Contact</a>
+    </nav>
+    
+    <main>
+        <section>
+            <article>
+
+            </article>
+        </section>
+
+        <section>
+            <article>
+                
+            </article>
+        </section>
+
+        <section>
+            <article>
+                
+            </article>
+        </section>
+
+        <section>
+            <article>
+                
+            </article>
+        </section>
+    </main>
 </body>
 
 </html>
