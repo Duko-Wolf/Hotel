ae32a3069c4ee3e494b5f8e85eba7456bf099b07
Duko Wolf <dukowolf@gmail.com>
2025-02-12 12:57:16 +0100

    fix qua style koppeling

--- ons.php
+++ ons.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styling/index.css"/>
+    <link rel="stylesheet" href="styling/index.css"/>
     <title>Overons</title>
 </head>
 <body>
--- weer.php
+++ weer.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styling/index.css"/>
+    <link rel="stylesheet" href="styling/index.css"/>
     <title>Weerstation Den Hoorn, Texel</title>
     <?php include('includes/navbar.php'); ?>
     <style>
