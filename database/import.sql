d2016791308b7f2e64165fcfa31c118b52833f89
Duko Wolf <dukowolf@gmail.com>
2025-02-14 14:28:28 +0100

    update

--- database/import.sql
+++ database/import.sql
@@ -33,25 +33,24 @@ VALUES
         'Comfort Kamer',
         'Onze Comfort Kamer biedt een serene ontsnapping met alle basisvoorzieningen die u nodig heeft. Geniet van een comfortabel bed, een moderne badkamer en een prachtig uitzicht op de stad. Perfect voor een kort verblijf of een zakenreis.',
         0
-    );
+    ),
 
 (
     'Deluxe Kamer',
     'Voor degenen die net dat beetje extra comfort willen, is onze Deluxe Kamer de perfecte keuze. Deze kamers zijn ruimer en beschikken over luxere voorzieningen, zoals een zithoek en een Nespresso-apparaat. De ideale plek om te ontspannen na een dag vol ontdekkingen in Alkmaar.',
     0
-);
-
+),
 (
     'Junior Suite',
     'Onze Junior Suites bieden een ultieme combinatie van ruimte en luxe. Met een aparte woonkamer, een ruime badkamer en een balkon met een adembenemend uitzicht, is deze kamer perfect voor een romantisch uitje of een speciale gelegenheid.',
     0
-);
+),
 
 (
     'Familie Suite',
     'Speciaal ontworpen voor gezinnen, biedt onze Familie Suite voldoende ruimte en comfort voor iedereen. Deze suites beschikken over twee aparte slaapkamers, een ruime woonkamer en extra voorzieningen zoals een kitchenette en speelhoek voor de kinderen. De perfecte thuisbasis voor een onvergetelijke familievakantie.',
     0
-);
+),
 
 (
     'Bruidssuite',
