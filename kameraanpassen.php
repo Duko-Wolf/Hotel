15241f106f4ac2d67f4d6a5b7570c77be0843dc5
Robin-1ct <rvaneijndthoven@ictvanafmorgen.nl>
2025-02-18 11:55:01 +0100

    weg halen br
    br is weg

--- kameraanpassen.php
+++ kameraanpassen.php
@@ -13,23 +13,21 @@
     <?php include('includes/navbar.php'); ?>
     <?php include('includes/header.php'); ?>
 
-
-    ?>
     <main>
         <section>
             <article> 
                 <h2>Contactformulier</h2>
                 <form action="kameraanpassen.php" method="post" enctype="multipart/form-data">
-                    <label for="name">Kamernaam:</label><br>
-                    <input type="text" id="name" name="kamerNaam" required><br><br>
+                    <label for="name">Kamernaam:</label>
+                    <input type="text" id="name" name="kamerNaam" required>
 
-                    <label for="email">Kamer beschrijving:</label><br>
-                    <input type="text" id="email" name="kamerBeschrijving" required><br><br>
+                    <label for="email">Kamer beschrijving:</label>
+                    <input type="text" id="email" name="kamerBeschrijving" required>
 
-                    <input type="file" name="fileToUpload" id="fileToUpload"><br>
+                    <input type="file" name="fileToUpload" id="fileToUpload">
 
-                    <label for="question">Prijs:</label><br>
-                    <input type="text" name="prijs"></textarea><br><br>
+                    <label for="question">Prijs:</label>
+                    <input type="text" name="prijs"></textarea>
 
                     <input type="submit" value="verstuur" name="verstuur">
                 </form>
