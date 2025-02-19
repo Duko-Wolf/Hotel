107553c1a8b55f1066296101268b1dbb18e30874
Duko Wolf <dukowolf@gmail.com>
2025-02-19 12:17:47 +0100

    komt ie

--- Index.php
+++ Index.php
@@ -1,55 +1,69 @@
 <?php include('includes/session.php') ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styling/global.css"/>
+    <link rel="stylesheet" href="styling/global.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Varela+Round&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Varela+Round&display=swap"
+        rel="stylesheet">
     <title>Hotel</title>
 </head>
 
 <body class="varela-round-regular">
-<?php include('includes/navbar.php'); ?>
-<?php include('includes/header.php'); ?>
-    
-    <main>
-    <section>
-        <article> 
-            <h2>Welkom bij Hotel De Zonne Vallei</h2>
-            <p>Ontsnap aan de dagelijkse drukte en ontdek de rust en luxe van Hotel De Zonne Vallei, een 3-duimen hotel gelegen in het hart van Alkmaar. Ons hotel biedt een perfecte mix van comfort, gastvrijheid en adembenemende natuur. 
-                Of u nu voor een romantisch uitje, een familievakantie of een zakelijke bijeenkomst komt, ons hotel heeft precies wat u nodig heeft voor een onvergetelijk verblijf.</p>
-        </article> 
-    </section>
+    <?php include('includes/navbar.php'); ?>
+    <?php include('includes/header.php'); ?>
 
-    <section>
-        <article>
-            <h2>Ontdek Onze Kamers</h2>
-            <p>Of u nu op zoek bent naar een romantisch uitje, een familievakantie of een zakelijke bijeenkomst, onze stijlvolle en goed uitgeruste kamers bieden alles wat u nodig heeft voor een onvergetelijk verblijf. 
-                Geniet van moderne voorzieningen, comfortabele bedden en een prachtig uitzicht op de omgeving.</p>
-        </article> 
-    </section>
+    <main>
+        <section>
+            <article>
+                <h2>Welkom bij Hotel De Zonne Vallei</h2>
+                <p>Ontsnap aan de dagelijkse drukte en ontdek de rust en luxe van Hotel De Zonne Vallei, een 3-duimen
+                    hotel gelegen in het hart van Alkmaar. Ons hotel biedt een perfecte mix van comfort, gastvrijheid en
+                    adembenemende natuur.
+                    Of u nu voor een romantisch uitje, een familievakantie of een zakelijke bijeenkomst komt, ons hotel
+                    heeft precies wat u nodig heeft voor een onvergetelijk verblijf.</p>
+            </article>
+        </section>
 
         <section>
             <article>
-            <h2> Culinaire Verwennerij</h2>  
-            <p> Laat uw smaakpapillen prikkelen in ons restaurant, waar onze chef-kok met passie lokale ingrediënten omtovert tot culinaire meesterwerken. 
-                Van een uitgebreid ontbijt tot een intiem diner, elke maaltijd is een ervaring op zich. Ontdek ons menu en geniet van de smaken van Alkmaar.</p>
-            </article> 
+                <h2>Ontdek Onze Kamers</h2>
+                <p>Of u nu op zoek bent naar een romantisch uitje, een familievakantie of een zakelijke bijeenkomst,
+                    onze stijlvolle en goed uitgeruste kamers bieden alles wat u nodig heeft voor een onvergetelijk
+                    verblijf.
+                    Geniet van moderne voorzieningen, comfortabele bedden en een prachtig uitzicht op de omgeving.</p>
+            </article>
+        </section>
+
+        <section>
+            <article>
+                <h2> Culinaire Verwennerij</h2>
+                <p> Laat uw smaakpapillen prikkelen in ons restaurant, waar onze chef-kok met passie lokale ingrediënten
+                    omtovert tot culinaire meesterwerken.
+                    Van een uitgebreid ontbijt tot een intiem diner, elke maaltijd is een ervaring op zich. Ontdek ons
+                    menu en geniet van de smaken van Alkmaar.</p>
+            </article>
         </section>
 
         <article>
-            <h2> Ontdek de Omgeving</h2>  
-            <p> Hotel De Zonne Vallei ligt in het bruisende hart van Alkmaar, een stad die rijk is aan geschiedenis en cultuur. Verken de pittoreske straatjes, bewonder de eeuwenoude architectuur en bezoek de wereldberoemde kaasmarkt.
-                 Of u nu winkelt in de boetiekjes, geniet van een drankje op een van de vele terrassen of een ontspannen wandeling maakt langs de grachten, Alkmaar biedt voor ieder wat wils. Ontdek de schoonheid en charme van deze historische stad tijdens uw verblijf in ons hotel</p>
-            </article> 
+            <h2> Ontdek de Omgeving</h2>
+            <p> Hotel De Zonne Vallei ligt in het bruisende hart van Alkmaar, een stad die rijk is aan geschiedenis en
+                cultuur. Verken de pittoreske straatjes, bewonder de eeuwenoude architectuur en bezoek de wereldberoemde
+                kaasmarkt.
+                Of u nu winkelt in de boetiekjes, geniet van een drankje op een van de vele terrassen of een ontspannen
+                wandeling maakt langs de grachten, Alkmaar biedt voor ieder wat wils. Ontdek de schoonheid en charme van
+                deze historische stad tijdens uw verblijf in ons hotel</p>
+        </article>
         </section>
-    </main> 
- <?php include('includes/footer.php'); ?> 
+    </main>
+    <?php include('includes/footer.php'); ?>
 </body>
 
 </html>
